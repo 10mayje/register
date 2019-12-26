1ec4a964bb0301500968cde30fa1ec9133fb562f
10mayje <52908397+10mayje@users.noreply.github.com>
2019-12-26 22:39:50 +0530

    Update views.py

--- website/account/views.py
+++ website/account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
-from .forms import CreateNewList,UserRegisterForm
+
 from django.contrib import  messages
 from django.contrib.auth.decorators import login_required
 posts=[
@@ -28,9 +28,7 @@ def home(request):
 def new(request):
     return render(request,"account/html.html")
 
-def create(request):
-    form=CreateNewList(request.POST)
-    return render(request, "account/create.html",form)
+
 
 def register(request):
     if request.method=='POST':
